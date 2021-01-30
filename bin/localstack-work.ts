6e9cfc349e075a1ccc1d059a1ecd243988074058
cm-sato-tomoki <sato.tomoki@classmethod.jp>
2021-01-30 15:51:01 +0900

    initial setting

--- bin/localstack-work.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { LocalstackWorkStack } from '../lib/localstack-work-stack';
-
-const app = new cdk.App();
-new LocalstackWorkStack(app, 'LocalstackWorkStack');
